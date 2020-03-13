d6da141d0fabbef8198c56ad2a873b24bcd714e3
Dan Trickey <dan@trickey.io>
2020-03-13 19:09:24 +0000

    Add Makefile, improve flash and add shell.

--- firmware/main.c
+++ firmware/main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
+
 #include "board.h"
 #include "xtimer.h"
 
+#include "shell.h"
+#include "msg.h"
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+
+void start_flash(void) {
+    for(int i = 0; i < 4; i++){
+        LED0_ON;
+	xtimer_usleep(50000);
+	LED0_OFF;
+	xtimer_usleep(50000);
+    }
+}
+
 int main(void)
 {
-    puts("RIOT USB stack example application");
+    puts("Remote Start Dongle.");
 
-    puts("Started USB stack!");
+    start_flash();    
+
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming networking packets */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    /* start shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
     
-    while(1) {
-        LED0_ON;
-	xtimer_usleep(200000);
-	LED0_OFF;
-	xtimer_usleep(200000);
-    }    
-
+    // Never reached.
     return 0;
 }
